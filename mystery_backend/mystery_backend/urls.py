5f93427f172967e277c875d0a1cdd3d3041c0588
cyberdeeb <abrahamdeeb@gmail.com>
2025-08-15 09:26:27 -0700

    Refactor static file settings for improved Railway deployment; add home view with API documentation links

--- mystery_backend/mystery_backend/urls.py
+++ mystery_backend/mystery_backend/urls.py
@@ -1,25 +1,31 @@
 """
 URL configuration for mystery_backend project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from game.views import CaseCreateAPIView, CaseDetailAPIView, GuessAPIView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+def home(request):
+    return HttpResponse("""
+    <html>
+    <head><title>Dead Giveaway Mystery API</title></head>
+    <body>
+        <h1>Dead Giveaway - Mystery Game API</h1>
+        <p>Welcome to the Dead Giveaway Mystery Game API!</p>
+        <ul>
+            <li><a href="/api/schema/swagger/">API Documentation (Swagger)</a></li>
+            <li><a href="/api/schema/redoc/">API Documentation (ReDoc)</a></li>
+            <li><a href="/admin/">Admin Panel</a></li>
+        </ul>
+    </body>
+    </html>
+    """)
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/cases/", CaseCreateAPIView.as_view()),
     path("api/cases/<int:pk>/", CaseDetailAPIView.as_view()),
